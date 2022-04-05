2314ca02d48d98662cfe30ffd19717d45ee70cd1
Keelan <keelanmatthews123@gmail.com>
2022-04-05 19:04:50 +0200

    first commit

---
+++ assignment 2/AvlTree.java
@@ -0,0 +1,48 @@
+public class AvlTree<T extends Comparable<T>> {
+    public Node<T> root;
+
+    public AvlTree() {
+        this.root = null;
+    }
+
+
+    int getHeight(Node<T> N) {
+        if (N == null)
+            return 0;
+
+        return N.height;
+    }
+
+    /*Printing AvlTree in inorder*/
+    void print(Node<T> node) {
+        if (node == null)
+            return;
+
+        print(node.left);
+
+        System.out.print(node.data + " ");
+
+        print(node.right);
+    }
+
+    /* Do not edit the code above */
+
+    /**
+     * Insert the given data into the tree.
+     * Duplicate data is not allowed. just return the node.
+     */
+
+    Node<T> insert(Node<T> node, T data) {
+    }
+
+
+    /**
+     * Remove / Delete the node based on the given data
+     * Return the node / root if the data do not exist
+     */
+
+    Node<T> removeNode(Node<T> root, T data) {
+
+    }
+
+}
